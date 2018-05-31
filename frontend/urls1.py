02b389bd814182f9858d2b34f3e7a032a17fb525
Akaisorani <1224067801@qq.com>
2018-05-31 16:19:37 +0800

    init views1

--- frontend/urls1.py
+++ frontend/urls1.py
@@ -1,5 +1,9 @@
 from django.urls import path
 import frontend.views1 as views
 urlpatterns = [
-    path('test1/', views.test),
+    #path('test1/', views.test),
+	path('view_applicants_list/', views.view_applicants_list),
+	path('score_applicants/', views.score_applicants),
+	path('edit_applicants_workflow/', views.edit_applicants_workflow),
+	path('release_job/', views.release_job),
 ]
