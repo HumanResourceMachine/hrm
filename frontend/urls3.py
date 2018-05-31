f8c4f16fc13032d6c14f33f11537bb06cfec402b
00yk <601455322@qq.com>
2018-05-31 17:50:31 +0800

    add login and register html.

--- frontend/urls3.py
+++ frontend/urls3.py
@@ -2,4 +2,8 @@ from django.urls import path
 import frontend.views3 as views
 urlpatterns = [
     path('test3/', views.test),
+    path('login/', views.login),
+    path('register/', views.register),
+    path('apply_job', views.apply_job),
+    path('view_job_info', views.view_job_info),
 ]
