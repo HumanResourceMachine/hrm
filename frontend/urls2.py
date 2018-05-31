e45c36e673e04bbbc93070631c5a419f368701c6
Yukun Ma <gmayukun@gmail.com>
2018-05-31 17:27:09 +0800

    initialize views2

--- frontend/urls2.py
+++ frontend/urls2.py
@@ -2,4 +2,8 @@ from django.urls import path
 import frontend.views2 as views
 urlpatterns = [
     path('test2/', views.test),
+    path('change_time/', views.change_time),
+    path('view_time/', views.view_time),
+    path('view_status/', views.view_status),
+    path('view_applicants/', views.view_applicants),
 ]
--- frontend/views2.py
+++ frontend/views2.py
@@ -5,3 +5,15 @@ from django.http import JsonResponse
 
 def test(request):
     return render(request, "test.html")
+
+def change_time(request):
+    return render(request, "change_time.html")
+
+def view_time(request):
+    return render(request, "view_time.html")
+
+def view_applicants(request):
+    return render(request, "view_applicants.html")
+
+def view_status(request):
+    return  render(request, "view_status.html")
