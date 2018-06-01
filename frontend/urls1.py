0ab41b9298b203959050b9e83d2f3c15fd568b1d
Akaisorani <1224067801@qq.com>
2018-06-02 01:07:16 +0800

    add releasejob scoreapp

--- frontend/urls1.py
+++ frontend/urls1.py
@@ -14,8 +14,8 @@ def testjson(request):
         "status": 0,
         "interviewer": "park",
         "date": "2018-06-01"
-	}
-	]*15})
+	}]*15
+	})
 
 urlpatterns = [
     #path('test1/', views.test),
@@ -23,5 +23,6 @@ urlpatterns = [
 	path('score_applicants/', views.score_applicants),
 	path('edit_applicants_workflow/', views.edit_applicants_workflow),
 	path('release_job/', views.release_job),
-	path('view_applicants_list/applicants_list',testjson)
+	#path('view_applicants_list/applicants_list',testjson),
+	#path('users/release_job',testjson)
 ]
